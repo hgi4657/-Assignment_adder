28a09f33e52f78255c99af646c4ac6f57d5e2fde
hgi4657 <hgi4657@naver.com>
2024-04-30 16:16:20 +0900

    Level 1-2

--- src/App.java
+++ src/App.java
@@ -8,5 +8,7 @@ public class App {
         int firstNum = sc.nextInt();
         System.out.print("두 번째 숫자를 입력하세요: ");
         int SecondNum = sc.nextInt();
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        char operator = sc.next().charAt(0);
     }
 }
