d18a5564662ed8755344fef5a854527c72693a7e
hgi4657 <hgi4657@naver.com>
2024-05-01 20:11:39 +0900

    Level 2-7

--- src/App.java
+++ src/App.java
@@ -14,36 +14,60 @@ public class App {
         int result;
 
         try {
-            while (!inputExit.equals("exit")) {
-                System.out.print("첫 번째 숫자를 입력하세요: ");
-                int firstNum = sc.nextInt();
-                System.out.print("두 번째 숫자를 입력하세요: ");
-                int secondNum = sc.nextInt();
-                System.out.print("사칙연산 기호를 입력하세요: ");
-                char operator = sc.next().charAt(0);
+            System.out.println("사칙연산을 계산하고 싶다면 '1'을 입력하고, 원의 넓이를 구하고 싶다면 '2'를 입력하세요");
+            String goForOperatorCircle = sc.nextLine();
 
-                result = calc.calculate(operator,firstNum,secondNum);
-                System.out.println("결과: " + result);
+            // 1 을 받았을 경우 (사칙연산)
+            if (goForOperatorCircle.equals("1")) {
+                while (!inputExit.equals("exit")) {
+                    System.out.print("첫 번째 숫자를 입력하세요: ");
+                    int firstNum = sc.nextInt();
+                    System.out.print("두 번째 숫자를 입력하세요: ");
+                    int secondNum = sc.nextInt();
+                    System.out.print("사칙연산 기호를 입력하세요: ");
+                    char operator = sc.next().charAt(0);
 
-                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-                inputRemove = sc.next();
+                    // 사칙연산 계산
+                    result = calc.calculate(operator,firstNum,secondNum);
+                    System.out.println("결과: " + result);
 
-                // remove 를 입력 받았을 때 수행
-                if (inputRemove.equals("remove")) {
-                    calc.removeResult();
+                    System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                    inputRemove = sc.next();
+
+                    // remove 를 입력 받았을 때 첫번째 index 값 삭제
+                    if (inputRemove.equals("remove")) {
+                        calc.removeResult();
+                    }
+
+                    System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
+                    intQuiry = sc.next();
+
+                    // inquiry 를 입력 받았을 때 전체 조회
+                    if (intQuiry.equals("inquiry")) {
+                        calc.inquiryResults();
+                    }
+
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    // 초기값 수정
+                    inputExit = sc.next();
                 }
+            // 2 를 받았을 경우 (원의 넓이)
+            } else if (goForOperatorCircle.equals("2")) {
+                while (!inputExit.equals("exit")) {
+                    System.out.print("원의 반지름을 입력하세요: ");
+                    int circleRadius = sc.nextInt();
 
-                System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
-                intQuiry = sc.next();
+                    // 계산된 원의 넓이
+                    System.out.println("결과: " + calc.calculateCircleArea(circleRadius));
 
-                // inquiry 를 입력 받았을 때 수행
-                if (intQuiry.equals("inquiry")) {
-                    calc.inquiryResults();
+                    // 저장된 원의 넓이 값들 전체 조회
+                    calc.inquiryAreas();
+
+                    System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+                    inputExit = sc.next();
                 }
-
-                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-                // 초기값 수정
-                inputExit = sc.next();
+            } else {
+                System.out.println("해당 숫자는 계산식이 존재하지 않습니다");
             }
         } catch (Exception e) {
             e.getMessage();
--- src/Calculator.java
+++ src/Calculator.java
@@ -5,14 +5,38 @@ public class Calculator {
 
     // 생성자 추가 / 컬렉션 초기화
     public Calculator() {
+        this.areaList = new ArrayList<>();
         this.numberList = new ArrayList<>();
     }
 
     // 배열 선언
     private ArrayList<Integer> numberList;
+    private ArrayList<Double> areaList;
     // 정규식 검사용
     private static final String OPERATION_REG = "[+\\-*/]";
+    // 원주율 저장 => static final : 공통적인 변하지 않는 불변의 값 (상수)
+    private static final double PI = 3.14;
 
+    // 원의 넓이를 계산하는 메서드
+    public double calculateCircleArea(int radius) {
+        double area = radius * radius * PI;
+        areaList.add(area);
+        return area;
+    }
+    public ArrayList<Double> getAreaList() {
+        return areaList;
+    }
+    public void setAreaList(ArrayList<Double> areaList) {
+        this.areaList = areaList;
+    }
+    public void inquiryAreas() {
+        for (double i : areaList) {
+            System.out.print(i + " ");
+        }
+        System.out.println(); // 줄바꿈
+    }
+
+    // 사칙연산 계산하는 메서드
     public int calculate(char operator, int firstNumber, int secondNumber) throws Exception {
         // 오류 검사 (사칙연산, 두번째 숫자 0)
         if (!Pattern.matches(OPERATION_REG, String.valueOf(operator))) {
